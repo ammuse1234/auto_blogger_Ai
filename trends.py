545661aa29f0571ce22ccdc83f9d7eb2a32dff44
ammuse1234 <raromero084@gmail.com>
2025-05-30 03:24:16 +0300

    Update trends.py

--- trends.py
+++ trends.py
@@ -1,7 +1,10 @@
-from pytrends.request import TrendReq
+import requests
+from bs4 import BeautifulSoup
 
 def get_trending_topics():
-    pytrends = TrendReq()
-    pytrends.build_payload(kw_list=["news"])
-    trending_searches = pytrends.trending_searches()
-    return trending_searches[0:5].tolist()
+    url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=US"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "xml")
+    items = soup.find_all("item")
+    topics = [item.title.text for item in items]
+    return topics
