4616133790996241cf2c6631a76bb8e490cc16f9
ammuse1234 <raromero084@gmail.com>
2025-05-30 03:19:39 +0300

    Update trends.py

--- trends.py
+++ trends.py
@@ -3,5 +3,5 @@ from pytrends.request import TrendReq
 def get_trending_topics():
     pytrends = TrendReq()
     pytrends.build_payload(kw_list=["news"])
-    trending_searches = pytrends.trending_searches(pn="united_states")
+    trending_searches = pytrends.trending_searches()
     return trending_searches[0:5].tolist()
