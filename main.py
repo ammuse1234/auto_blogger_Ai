f96bad050353af065706bd0921cd7064404e8c9f
ammuse1234 <raromero084@gmail.com>
2025-05-30 03:59:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+
+# نقرأ من GitHub secret ونحوله إلى ملف مؤقت
+client_secret_content = os.environ.get("GOOGLE_CLIENT_SECRET_JSON")
+
+if client_secret_content:
+    with open("client_secret.json", "w") as f:
+        f.write(client_secret_content)
+        
 from trends import get_trending_topics
 from article_generator import generate_article
 from blogger import post_to_blogger
