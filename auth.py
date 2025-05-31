e39064396c6d3b5f1fa13b0c21f8827d89e7604b
ammuse1234 <raromero084@gmail.com>
2025-05-31 14:13:30 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,11 +1,27 @@
 import os
-import json
-import requests
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
-def get_access_token():
-    creds = Credentials.from_authorized_user_file("token.json", ["https://www.googleapis.com/auth/blogger"])
-    if creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    return creds.token
+SCOPES = ['https://www.googleapis.com/auth/blogger']
+
+def get_authenticated_service():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('blogger', 'v3', credentials=creds)
+    return service
