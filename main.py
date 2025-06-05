fd7bbe8a856f68f0b849b707ed04936545c7eace
ammuse1234 <raromero084@gmail.com>
2025-06-05 14:29:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def get_access_token():
 
 # دالة توليد المقال باستخدام Gemini
 def generate_article(topic: str) -> str:
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=GEMINI_API_KEY"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDSOgakd0CgLzG0h8C1ZXIjMV7OavNax9c"
 
     headers = {
         "Content-Type": "application/json"
