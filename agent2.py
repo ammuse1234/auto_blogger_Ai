43a69cdda93ebe9b7916dbb7b6daed8b1d2f90ff
ammuse1234 <raromero084@gmail.com>
2025-07-03 02:53:58 +0300

    Update agent2.py

--- agent2.py
+++ agent2.py
@@ -4,7 +4,7 @@ import requests
 from message_generator import generate_message
 from get_articles import get_articles
 from bs4 import BeautifulSoup
-import proxy_manager  # ملف إدارة البروكسيات
+import proxy_manager2 # ملف إدارة البروكسيات
 
 mastodon = MastodonPoster()
 
